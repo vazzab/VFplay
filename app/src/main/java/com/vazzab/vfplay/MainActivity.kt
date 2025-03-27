57b326506c005e3044b204c023e890729fdb6c72
vazzab <vazzyaaa@gmail.com>
2025-03-27 20:10:28 +0900

    added activity_main.xml

---
+++ app/src/main/java/com/vazzab/vfplay/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.vazzab.vfplay
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vazzab.vfplay.ui.theme.VFplayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
