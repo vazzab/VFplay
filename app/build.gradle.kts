e2a55b35e01a263eee390fb05e28ddee3ffacb34
vazzab <vazzyaaa@gmail.com>
2025-04-01 13:21:49 +0900

    1. Added menu buttons 2. Added movie posters 3. TextView 4. Applied Drawable for backgrounds 5. Applied CardView for rounded corners and elevation 6. Added Toast messages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/vazzab/vfplay/MainActivity.kt
+++ app/src/main/java/com/vazzab/vfplay/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.vazzab.vfplay
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.vazzab.vfplay.ui.theme.VFplayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttons = listOf(
+            R.id.button1 to R.string.navigation_button_main,
+            R.id.button2 to R.string.navigation_button_category,
+            R.id.button3 to R.string.navigation_button_bookmark,
+            R.id.button4 to R.string.navigation_button_new,
+            R.id.button5 to R.string.navigation_button_top
+        )
+
+        buttons.forEach { (buttonId, stringId) ->
+            findViewById<Button>(buttonId).setOnClickListener {
+                Toast.makeText(this, getString(stringId), Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
